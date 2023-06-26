a9b99ec73fe8b8cfc554f15d98755513535457db
Nico Schett <nicoschett@icloud.com>
2023-06-26 23:33:08 +0200

    WIP: update apiURL of clients for internal routing in production
    This commit updates the apiURL of clients to utilize internal routing in the production environment.
    By upgrading to the cli version 0.0.9 and server version 0.0.14, this enhancement becomes possible.
    
    The changes made in this commit will ensure that clients access the necessary APIs internally,
    optimizing the routing process and improving overall performance.
    
    Note: Upgrading to cli version 0.0.9 and server version 0.0.14 is required
    for this update to work correctly.

--- src/clients/mailer/src/index.ts
+++ src/clients/mailer/src/index.ts
@@ -1,9 +1,14 @@
 import { makeSnekQuery } from "snek-query";
 import { Query, Mutation } from "./schema.generated.js";
 
+const apiURL =
+  process.env.NODE_ENV === "production"
+    ? "http://mailer:3000/graphql"
+    : "https://services.snek.at/mailer/graphql";
+
 export const sq = makeSnekQuery(
   { Query, Mutation },
   {
-    apiURL: "https://services.snek.at/mailer/graphql",
+    apiURL,
   }
 );
